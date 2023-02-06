920a6c0d516a91ffeb9955f0c3de183cb53d96a2
Daniel Nygaard <dani12h1@stud.kea.dk>
2023-02-06 09:53:19 +0100

    template literals done

---
+++ strings.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// const name = "Daniel";
+// const drink = "cola-cola";
+
+// console.log(`hello ${name}, would you like a ${drink}?`);
+
+// const text = console.log(`hello ${name}, would you like a ${drink}?`);
+
+// console.log(text);
+
+// console.log("Hello " + name + ", would to like a " + drink);
+
+const name = "Daniel";
+const cat = "Ejvind";
+const animalType = "cat";
+
+const text = console.log(`My name is ${name}. \nI have a ${animalType} called ${cat}.`);
